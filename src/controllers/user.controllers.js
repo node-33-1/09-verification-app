105ec40745b2fd676a78970b063e0b6922a140f8
andres <andres.david.mm@hotmail.com>
2024-03-08 22:56:17 -0500

    added login

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,6 +3,7 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const sendEmail = require('../utils/sendEmail');
 const EmailCode = require('../models/EmailCode');
+const jwt = require('jsonwebtoken');
 
 const getAll = catchError(async(req, res) => {
     const results = await User.findAll();
@@ -76,6 +77,29 @@ const verifyEmail = catchError(async(req, res) => {
     return res.json(user[1][0]);
 });
 
+const login = catchError(async(req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email: email } });
+    if (!user) return res.status(401).json({ message: "Credenciales incorrectas" });
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) return res.status(401).json({ message: "Credenciales incorrectas" });
+    if (user.isVerified === false) 
+        return res.status(401).json({ message: "Usuario sin verificar" });
+
+    const token = jwt.sign(
+        { user },
+        process.env.TOKEN_SECRET,
+        { expiresIn: '1d' },
+    );
+
+    return res.json({user, token});
+
+});
+
+const getLoggedUser = catchError(async(req, res) => {
+    return res.json(req.user);
+});
+
 module.exports = {
     getAll,
     create,
@@ -83,4 +107,6 @@ module.exports = {
     remove,
     update,
     verifyEmail,
+    login,
+    getLoggedUser,
 }
